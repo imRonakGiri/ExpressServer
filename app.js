93f7b032eb8956714e0c9b1f15d46952a1d588d0
RonakGiri A Goswami <goswamironakgiri38@gmail.com>
2021-11-25 13:18:54 +0530

    day 2 update 1.2

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const port = process.env.PORT || 3000;
 import express from "express";
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("hey Ronak Goswami I'm Your Own Coded Express Server ...");
-});
+app.use(express.static("public"));
 
 app.listen(port, () => console.log(`port ${port} is Now Running...`));
